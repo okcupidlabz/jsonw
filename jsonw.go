24ea07683e5c272228415a17729c7f0434e3b9f6
Maxwell Krohn <themax@gmail.com>
2012-03-14 21:49:23 -0400

    gofmt'ing

--- jsonw.go
+++ jsonw.go
@@ -1,287 +1,283 @@
-// -*- mode: go; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
-
 package jsonw
 
 import (
-    "fmt"
-    "reflect"
+	"fmt"
+	"reflect"
 )
 
-
 type Wrapper struct {
-    dat interface{}
-    err *Error
+	dat interface{}
+	err *Error
 }
 
 type Error struct {
-    msg string
+	msg string
 }
 
-func (e Error) Error() string { return e.msg; }
+func (e Error) Error() string { return e.msg }
 
-func wrongType (w string, g reflect.Kind) *Error {
-    return &Error { fmt.Sprintf("type error: wanted %s, got %s", w, g) }
+func wrongType(w string, g reflect.Kind) *Error {
+	return &Error{fmt.Sprintf("type error: wanted %s, got %s", w, g)}
 }
 
 func (i *Wrapper) getData() interface{} { return i.dat }
-func (i *Wrapper) IsOk() bool { return i.Error() == nil; }
+func (i *Wrapper) IsOk() bool           { return i.Error() == nil }
 
-func (i *Wrapper) GetData() (dat interface{}, err error) { 
-    if i.err != nil {
-        err = *i.err
-    } else {
-        dat = i.dat
-    }
-    return 
+func (i *Wrapper) GetData() (dat interface{}, err error) {
+	if i.err != nil {
+		err = *i.err
+	} else {
+		dat = i.dat
+	}
+	return
 }
 
-func (i *Wrapper) Error() (e error) { 
-    if i.err != nil {
-        e = *i.err
-    }
-    return
+func (i *Wrapper) Error() (e error) {
+	if i.err != nil {
+		e = *i.err
+	}
+	return
 }
 
-func (i *Wrapper) GetDataOrNil() interface{} { return i.getData(); }
+func (i *Wrapper) GetDataOrNil() interface{} { return i.getData() }
 
-func NewWrapper (i interface{}) (rd *Wrapper) {
-    rd = new (Wrapper);
-    rd.dat = i;
-    return rd;
+func NewWrapper(i interface{}) (rd *Wrapper) {
+	rd = new(Wrapper)
+	rd.dat = i
+	return rd
 }
 
 func NewDictionary() *Wrapper {
-    m := make (map[string]interface{})
-    return NewWrapper (m);
+	m := make(map[string]interface{})
+	return NewWrapper(m)
 }
 
 func NewArray(l int) *Wrapper {
-    m := make ([]interface{}, l)
-    return NewWrapper (m);
+	m := make([]interface{}, l)
+	return NewWrapper(m)
 }
 
 func NewNil() *Wrapper {
-    return NewWrapper(nil);
+	return NewWrapper(nil)
 }
 
 func NewInt(i int) *Wrapper {
-    return NewWrapper(i);
+	return NewWrapper(i)
 }
 
 func NewInt64(i int64) *Wrapper {
-    return NewWrapper (i);
+	return NewWrapper(i)
 }
 
-func NewUint64 (u uint64) *Wrapper {
-    return NewWrapper(u)
+func NewUint64(u uint64) *Wrapper {
+	return NewWrapper(u)
 }
 
-func NewString (s string) *Wrapper {
-    return NewWrapper(s);
+func NewString(s string) *Wrapper {
+	return NewWrapper(s)
 }
 
-func NewBool (b bool) *Wrapper {
-    return NewWrapper(b);
+func NewBool(b bool) *Wrapper {
+	return NewWrapper(b)
 }
 
 func isInt(v reflect.Value) bool {
-  k := v.Kind()
-  return k == reflect.Int || k == reflect.Int8 || 
-        k == reflect.Int16 || k == reflect.Int32 || 
-        k == reflect.Int64
+	k := v.Kind()
+	return k == reflect.Int || k == reflect.Int8 ||
+		k == reflect.Int16 || k == reflect.Int32 ||
+		k == reflect.Int64
 }
 
 func isUint(v reflect.Value) bool {
-  k := v.Kind()
-  return k == reflect.Uint || k == reflect.Uint8 || 
-        k == reflect.Uint16 || k == reflect.Uint32 || 
-        k == reflect.Uint64
+	k := v.Kind()
+	return k == reflect.Uint || k == reflect.Uint8 ||
+		k == reflect.Uint16 || k == reflect.Uint32 ||
+		k == reflect.Uint64
 }
 
 func isFloat(v reflect.Value) bool {
-    k := v.Kind()
-    return k == reflect.Float32 || k == reflect.Float64
+	k := v.Kind()
+	return k == reflect.Float32 || k == reflect.Float64
 }
 
 func (rd *Wrapper) GetInt() (ret int64, err error) {
-    if rd.err != nil {
-        err = rd.err;
-    } else {
-        v := reflect.ValueOf (rd.dat)
-        if isInt (v) {
-            ret = v.Int()
-        } else if isFloat (v) {
-            ret = int64(v.Float())
-        } else if ! isUint (v) {
-            err = wrongType ("int", v.Kind());
-        } else if v.Uint() <= (1<<63 - 1) {
-            ret = int64(v.Uint());
-        } else {
-            err = Error { "Signed int64 overflow error" }
-        }
-    }
-    return 
+	if rd.err != nil {
+		err = rd.err
+	} else {
+		v := reflect.ValueOf(rd.dat)
+		if isInt(v) {
+			ret = v.Int()
+		} else if isFloat(v) {
+			ret = int64(v.Float())
+		} else if !isUint(v) {
+			err = wrongType("int", v.Kind())
+		} else if v.Uint() <= (1<<63 - 1) {
+			ret = int64(v.Uint())
+		} else {
+			err = Error{"Signed int64 overflow error"}
+		}
+	}
+	return
 }
 
 func (rd *Wrapper) GetUint() (ret uint64, err error) {
-    if rd.err != nil {
-        err = rd.err;
-    } else {
-        underflow := false
-        v := reflect.ValueOf (rd.dat)
-        if isUint (v) {
-            ret = v.Uint()
-        } else if isFloat (v) {
-            if v.Float() <= 0 {
-                underflow = true;
-            } else {
-                ret = uint64(v.Float())
-            }
-        } else if ! isInt (v) {
-            err = wrongType ("uint", v.Kind());
-        } else if v.Int() >= 0 {
-            ret = uint64(v.Int());
-        } else {
-            underflow = true
-        }
+	if rd.err != nil {
+		err = rd.err
+	} else {
+		underflow := false
+		v := reflect.ValueOf(rd.dat)
+		if isUint(v) {
+			ret = v.Uint()
+		} else if isFloat(v) {
+			if v.Float() <= 0 {
+				underflow = true
+			} else {
+				ret = uint64(v.Float())
+			}
+		} else if !isInt(v) {
+			err = wrongType("uint", v.Kind())
+		} else if v.Int() >= 0 {
+			ret = uint64(v.Int())
+		} else {
+			underflow = true
+		}
 
-        if underflow {
-            err = Error { "Unsigned uint64 underflow error" }
+		if underflow {
+			err = Error{"Unsigned uint64 underflow error"}
 
-        }
-    }
-    return
+		}
+	}
+	return
 }
 
 func (rd *Wrapper) GetBool() (ret bool, err error) {
-    if rd.err != nil {
-        err = rd.err
-    } else {
-        v := reflect.ValueOf (rd.dat)
-        k := v.Kind()
-        if k == reflect.Bool {
-            ret = v.Bool();
-        } else {
-            err = wrongType("bool", k);
-        }
-    }
-    return
+	if rd.err != nil {
+		err = rd.err
+	} else {
+		v := reflect.ValueOf(rd.dat)
+		k := v.Kind()
+		if k == reflect.Bool {
+			ret = v.Bool()
+		} else {
+			err = wrongType("bool", k)
+		}
+	}
+	return
 }
 
 func (rd *Wrapper) GetString() (ret string, err error) {
-    if rd.err != nil {
-        err = rd.err;
-    } else if v := reflect.ValueOf (rd.dat); v.Kind() == reflect.String {
-        ret = v.String();
-    } else if b, ok := rd.dat.([]uint8); ok {
-        ret = string(b);
-    } else if b, ok := rd.dat.([]byte); ok {
-        ret = string (b);
-    } else {
-        err = wrongType("string", v.Kind());
-    }
-    return
+	if rd.err != nil {
+		err = rd.err
+	} else if v := reflect.ValueOf(rd.dat); v.Kind() == reflect.String {
+		ret = v.String()
+	} else if b, ok := rd.dat.([]uint8); ok {
+		ret = string(b)
+	} else if b, ok := rd.dat.([]byte); ok {
+		ret = string(b)
+	} else {
+		err = wrongType("string", v.Kind())
+	}
+	return
 }
 
 func (rd *Wrapper) AtIndex(i int) *Wrapper {
-    ret, v := rd.asArray()
-    if v == nil {
+	ret, v := rd.asArray()
+	if v == nil {
 
-    } else if len (v) <= i {
-        m := fmt.Sprintf ("index out of bounds %d >= %d", i, len(v))
-        ret.err = &Error { m };
-    } else {
-        ret.dat = v[i];
-    }
-    return ret;
+	} else if len(v) <= i {
+		m := fmt.Sprintf("index out of bounds %d >= %d", i, len(v))
+		ret.err = &Error{m}
+	} else {
+		ret.dat = v[i]
+	}
+	return ret
 }
 
 func (rd *Wrapper) Len() (ret int, err error) {
-    tmp, v := rd.asArray()
-    if v == nil {
-        err = tmp.err
-    } else {
-        ret = len(v);
-    }
-    return
+	tmp, v := rd.asArray()
+	if v == nil {
+		err = tmp.err
+	} else {
+		ret = len(v)
+	}
+	return
 }
 
 func (i *Wrapper) Keys() (v []string, err error) {
-    tmp, d := i.asDictionary()
-    if d == nil {
-      err = tmp.err;
-    } else {
-      v = make([]string, len(d));
-      var i int = 0;
-      for k,_ := range d {
-        v[i] = k
-        i++
-      }
-    }
-    return
+	tmp, d := i.asDictionary()
+	if d == nil {
+		err = tmp.err
+	} else {
+		v = make([]string, len(d))
+		var i int = 0
+		for k, _ := range d {
+			v[i] = k
+			i++
+		}
+	}
+	return
 }
 
 func (i *Wrapper) asArray() (ret *Wrapper, v []interface{}) {
-    if i.err != nil {
-        ret = i;
-    } else {
-        var ok bool
-        v, ok = (i.dat).([]interface{});
-        ret = new(Wrapper);
-        if !ok {
-            ret.err = wrongType ("array", reflect.ValueOf(i.dat).Kind());
-        }
-    }
-    return
+	if i.err != nil {
+		ret = i
+	} else {
+		var ok bool
+		v, ok = (i.dat).([]interface{})
+		ret = new(Wrapper)
+		if !ok {
+			ret.err = wrongType("array", reflect.ValueOf(i.dat).Kind())
+		}
+	}
+	return
 }
 
 func (rd *Wrapper) IsNil() bool {
-    return rd.dat == nil;
+	return rd.dat == nil
 }
 
 func (rd *Wrapper) AtKey(s string) *Wrapper {
-    ret, d := rd.asDictionary()
+	ret, d := rd.asDictionary()
 
-    if d != nil {
-        val,found := d[s];
-        if found {
-            ret.dat = val;
-        } else {
-            ret.dat = nil;
-        }
-    }
-    return ret;
+	if d != nil {
+		val, found := d[s]
+		if found {
+			ret.dat = val
+		} else {
+			ret.dat = nil
+		}
+	}
+	return ret
 }
 
 func (w *Wrapper) SetKey(s string, val *Wrapper) error {
-    b, d := w.asDictionary()
-    if d != nil {
-        d[s] = val.getData()
-    }
-    return b.Error()
+	b, d := w.asDictionary()
+	if d != nil {
+		d[s] = val.getData()
+	}
+	return b.Error()
 }
 
-func (w *Wrapper) SetIndex (i int, val *Wrapper) error {
-    b, d := w.asArray()
-    if d != nil {
-        d[i] = val.getData()
-    }
-    return b.Error()
+func (w *Wrapper) SetIndex(i int, val *Wrapper) error {
+	b, d := w.asArray()
+	if d != nil {
+		d[i] = val.getData()
+	}
+	return b.Error()
 
 }
 
 func (i *Wrapper) asDictionary() (ret *Wrapper, d map[string]interface{}) {
-    if i.err != nil {
-        ret = i
-    } else {
-        var ok bool
-        d, ok = (i.dat).(map[string]interface{});
-        ret = new (Wrapper);
-        if !ok {
-            fmt.Printf("type gotten: %s", reflect.ValueOf(i.dat).String())
-            ret.err = wrongType ("dict", reflect.ValueOf(i.dat).Kind());
-        }
-    }
-    return 
+	if i.err != nil {
+		ret = i
+	} else {
+		var ok bool
+		d, ok = (i.dat).(map[string]interface{})
+		ret = new(Wrapper)
+		if !ok {
+			fmt.Printf("type gotten: %s", reflect.ValueOf(i.dat).String())
+			ret.err = wrongType("dict", reflect.ValueOf(i.dat).Kind())
+		}
+	}
+	return
 }
-
--- jsonw_test.go
+++ jsonw_test.go
@@ -1,84 +1,80 @@
-// -*- mode: go; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
-
 package jsonw
 
 import (
-    "testing"
-    "bytes"
+	"bytes"
+	"testing"
 )
 
-func TestInt (t *testing.T) {
-	const x = 100;
-	w := NewInt(x);
-	if v, _ := w.GetInt(); v != x  {
-		t.Errorf("%d != %d in GetInt() test", v, x);
+func TestInt(t *testing.T) {
+	const x = 100
+	w := NewInt(x)
+	if v, _ := w.GetInt(); v != x {
+		t.Errorf("%d != %d in GetInt() test", v, x)
 	}
 }
 
-
-func TestBigInt (t *testing.T) {
-	const x = 1 << 62 + 55555;
-	w := NewInt64(x);
-	if v, _ := w.GetInt(); v != x  {
-		t.Errorf("Big int test failed");
+func TestBigInt(t *testing.T) {
+	const x = 1<<62 + 55555
+	w := NewInt64(x)
+	if v, _ := w.GetInt(); v != x {
+		t.Errorf("Big int test failed")
 	}
-    if v, _ := w.GetUint(); v != x {
-		t.Errorf("Big uint test failed");
-    }
+	if v, _ := w.GetUint(); v != x {
+		t.Errorf("Big uint test failed")
+	}
 }
 
-func TestBytes (t *testing.T) {
-    s := "hello world";
-    buf := bytes.NewBufferString(s)
-    bv := buf.Bytes()
+func TestBytes(t *testing.T) {
+	s := "hello world"
+	buf := bytes.NewBufferString(s)
+	bv := buf.Bytes()
 
-    w := NewWrapper(bv)
-    if out, err := w.GetString(); err != nil || out != s {
-        t.Errorf("failed to get %s back out", s)
-    }
-    
+	w := NewWrapper(bv)
+	if out, err := w.GetString(); err != nil || out != s {
+		t.Errorf("failed to get %s back out", s)
+	}
+
 }
 
-func TestDict (t *testing.T) {
-    w := NewDictionary()
-    const dog = 3333
-    var cat string = "meow"
-    
-    w.SetKey("dog", NewInt(dog));
-    w.SetKey("cat", NewString(cat));
+func TestDict(t *testing.T) {
+	w := NewDictionary()
+	const dog = 3333
+	var cat string = "meow"
 
-    if v, _ := w.AtKey("dog").GetInt(); v != dog {
-        t.Errorf("Dictionary fail for 'dog': %d != %d", v, dog);
-    }
+	w.SetKey("dog", NewInt(dog))
+	w.SetKey("cat", NewString(cat))
 
-    if v, _ := w.AtKey("cat").GetString(); v != cat {
-        t.Errorf("Dictionary fail for 'dog': %s != %s", v, cat);
-    }
+	if v, _ := w.AtKey("dog").GetInt(); v != dog {
+		t.Errorf("Dictionary fail for 'dog': %d != %d", v, dog)
+	}
 
-    const parrot = 3318
-    var sparrow string = "tweet"
-    
-    w.SetKey("birds", NewDictionary());
-    w.AtKey("birds").SetKey("parrot", NewInt(parrot));
-    w.AtKey("birds").SetKey("sparrow", NewString(sparrow));
+	if v, _ := w.AtKey("cat").GetString(); v != cat {
+		t.Errorf("Dictionary fail for 'dog': %s != %s", v, cat)
+	}
 
-    if v,_ := w.AtKey("birds").AtKey("sparrow").GetString(); v != sparrow {
-        t.Errorf("Dictionary fail for birds.sparrow: %s != %s", v, sparrow);
-    }
-    if v,_ := w.AtKey("birds").AtKey("parrot").GetInt(); v != parrot {
-        t.Errorf("Dictionary fail for birds.sparrow: %d != %d", v, parrot);
-    }
+	const parrot = 3318
+	var sparrow string = "tweet"
 
-    w.AtKey("birds").SetKey("waterfowl", NewArray(2));
-    w.AtKey("birds").AtKey("waterfowl").SetIndex(0, NewString("duck"))
-    w.AtKey("birds").AtKey("waterfowl").SetIndex(1, NewString("swan"))
+	w.SetKey("birds", NewDictionary())
+	w.AtKey("birds").SetKey("parrot", NewInt(parrot))
+	w.AtKey("birds").SetKey("sparrow", NewString(sparrow))
 
-    if v,_ := w.AtKey("birds").AtKey("waterfowl").Len(); v != 2 {
-        t.Errorf("Wrong length for birds.waterfowl: %d v %d", v, 2);
-    }
+	if v, _ := w.AtKey("birds").AtKey("sparrow").GetString(); v != sparrow {
+		t.Errorf("Dictionary fail for birds.sparrow: %s != %s", v, sparrow)
+	}
+	if v, _ := w.AtKey("birds").AtKey("parrot").GetInt(); v != parrot {
+		t.Errorf("Dictionary fail for birds.sparrow: %d != %d", v, parrot)
+	}
 
-    if v,_ := w.AtKey("birds").AtKey("waterfowl").AtIndex(1).GetString(); v != "swan" {
-        t.Errorf ("Wrong waterfowl in array: %s v swan (%s)", v);
-    }
+	w.AtKey("birds").SetKey("waterfowl", NewArray(2))
+	w.AtKey("birds").AtKey("waterfowl").SetIndex(0, NewString("duck"))
+	w.AtKey("birds").AtKey("waterfowl").SetIndex(1, NewString("swan"))
+
+	if v, _ := w.AtKey("birds").AtKey("waterfowl").Len(); v != 2 {
+		t.Errorf("Wrong length for birds.waterfowl: %d v %d", v, 2)
+	}
+
+	if v, _ := w.AtKey("birds").AtKey("waterfowl").AtIndex(1).GetString(); v != "swan" {
+		t.Errorf("Wrong waterfowl in array: %s v swan (%s)", v)
+	}
 }
-
